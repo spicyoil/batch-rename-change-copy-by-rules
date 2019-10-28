533c7f2559eac53b3c8f34c8fe8965a8474db3fc
spicyoil <zyj0818@gmail.com>
2019-10-28 18:43:49 +0900

    Add files via upload

--- change_contents_script.py
+++ change_contents_script.py
@@ -16,7 +16,14 @@ if __name__ == "__main__":
     for each_file in searchFile(sys.argv[1]):
         print(each_file)
 
-    
+    if sys.argv[2]=="replace":
+            confirm = input("\nAre you sure to do the \" {0},{1},{2} \" to files above???(Y/N)".format(sys.argv[2],sys.argv[3],sys.argv[4])) 
+            if (confirm != "Y"):
+                exit()
+    elif sys.argv[2]=="delete":
+            confirm = input("\nAre you sure to do the \" {0},{1} \" to files above???(Y/N)".format(sys.argv[2],sys.argv[3])) 
+            if (confirm != "Y"):
+                exit()
 
     #print(sys.argv[1])
     for each_file in searchFile(sys.argv[1]):
@@ -28,15 +35,9 @@ if __name__ == "__main__":
 
         new_text=""
         if sys.argv[2]=="replace":
-            confirm = input("\nAre you sure to do the \" {0},{1},{2} \" to files above???(Y/N)".format(sys.argv[2],sys.argv[3],sys.argv[4])) 
-            if (confirm != "Y"):
-                exit()
             new_text= old_text.replace(sys.argv[4],sys.argv[3])
 
         elif sys.argv[2]=="delete":
-            confirm = input("\nAre you sure to do the \" {0},{1} \" to files above???(Y/N)".format(sys.argv[2],sys.argv[3])) 
-            if (confirm != "Y"):
-                exit()
             new_text=old_text.replace(sys.argv[3],"")
         
         #print(old_text,new_text)
