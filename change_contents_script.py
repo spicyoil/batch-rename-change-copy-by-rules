ae64f87e0463f9c5d30810489a754bc09b1744ae
spicyoil <zyj0818@gmail.com>
2019-10-28 16:15:22 +0900

    Add files via upload

---
+++ change_contents_script.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+#usage: .py target_keyword  replace  newword oldword
+#usage: .py target_keyword  delete   newword 
+import os,sys
+if hasattr(__builtins__, 'raw_input'):
+    input=raw_input
+
+def searchFile(target):
+    os.chdir(os.curdir)
+    for each_file in os.listdir(os.curdir):
+       if target in each_file:
+           yield each_file
+
+if __name__ == "__main__":
+
+    for each_file in searchFile(sys.argv[1]):
+        print(each_file)
+
+    
+
+    #print(sys.argv[1])
+    for each_file in searchFile(sys.argv[1]):
+        f = open(each_file,"r+")
+        old_text = f.read()        
+
+        f.seek(0) #set point to the head
+        f.truncate() #clear f
+
+        new_text=""
+        if sys.argv[2]=="replace":
+            confirm = input("\nAre you sure to do the \" {0},{1},{2} \" to files above???(Y/N)".format(sys.argv[2],sys.argv[3],sys.argv[4])) 
+            if (confirm != "Y"):
+                exit()
+            new_text= old_text.replace(sys.argv[4],sys.argv[3])
+
+        elif sys.argv[2]=="delete":
+            confirm = input("\nAre you sure to do the \" {0},{1} \" to files above???(Y/N)".format(sys.argv[2],sys.argv[3])) 
+            if (confirm != "Y"):
+                exit()
+            new_text=old_text.replace(sys.argv[3],"")
+        
+        #print(old_text,new_text)
+        #fnew= open("new","w")
+        f.write(new_text)
+        f.close()
+
+    
+
+
+
---
+++ copy_file_script.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*- 
+#usage: .py target_keyword  replace  newword oldword 
+#usage: .py target_keyword  delete   newword 
+#usage: .py target_keyword  add      newword 
+import os,sys,shutil 
+if hasattr(__builtins__, 'raw_input'):
+    input=raw_input
+
+def searchFile(target):
+    os.chdir(os.curdir)
+    for each_file in os.listdir(os.curdir):
+       if target in each_file:
+           yield each_file
+
+if __name__ == "__main__":
+
+    for each_file in searchFile(sys.argv[1]):
+        print(each_file)
+
+    confirm = input("\nAre you sure to change names of files above???(Y/N)") 
+    
+    if (confirm != "Y"):
+        exit()
+
+    #print(sys.argv[1])
+    for each_file in searchFile(sys.argv[1]):
+        new_file=""
+        if sys.argv[2]=="replace":
+            new_file = each_file.replace(sys.argv[4],sys.argv[3])
+        elif sys.argv[2]=="delete":
+            new_file = each_file.replace(sys.argv[3],"")
+        elif sys.argv[2]=="add":
+            new_file = each_file + sys.argv[3]
+        print(each_file,">",new_file)
+        shutil.copyfile(each_file,new_file)
